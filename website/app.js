d06d4d0370b206e0cdc06dfe7184e916de5a1a85
Felix Weickert <felix.weickert@dws.co.at>
2020-07-08 08:40:17 +0200

    feat: apiKey added

--- website/app.js
+++ website/app.js
@@ -1,5 +1,5 @@
 /* Global Variables */
-
+const apiKey = "f44cb97f3f4f45c41e014d003b3551e7"
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
